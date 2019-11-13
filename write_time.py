590b1d7c675848e5a450a18b66f4b66ab3ae7417
Zach Sharrow <sharrowz@gmail.com>
2019-11-13 15:14:18 -0500

    testing hits

--- write_time.py
+++ write_time.py
@@ -1,4 +1,24 @@
 import time
+import RPi.GPIO as GPIO
+import os
 
-with open("gate_test.txt", "a", encoding = "UTF-8") as data:
-	data.write(str(time.time()) + '\n')
+#Set the working directory
+os.chdir("/home/pi/Documents/gatecount")
+
+#Set up the I/O
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(7, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+
+#Startup confirmation (and time to move away from the sensor)
+print("Ready to go!")
+time.sleep(10)
+print("Waiting...")
+
+#Execution
+while True:
+	if GPIO.input(7):
+		print("Movement detected!")
+		with open('hits_test.txt', 'a', encoding = "UTF-8") as data:
+			data.write(str(time.time()) + '\n')
+		time.sleep(3)
+	
